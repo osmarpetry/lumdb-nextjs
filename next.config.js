67a98d389ac728a13114d8d427a766d53035b9a6
osmarpetry <osmarpetry@gmail.com>
2020-11-01 17:36:58 -0300

    serverless on netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
+    target: "serverless",
     images: {
         deviceSizes: [320, 640, 768, 1024, 1600],
         domains: ['image.tmdb.org']
