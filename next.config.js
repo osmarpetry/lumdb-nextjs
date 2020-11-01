0d40833a575806f1885d11026b4e251d8a54740b
osmarpetry <osmarpetry@gmail.com>
2020-11-01 05:25:59 -0300

    pwa on nextjs OMG

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   images: {
     deviceSizes: [320, 640, 768, 1024, 1600],
-    domains: ['image.tmdb.org']
-  }
-}
+    domains: ['image.tmdb.org'],
+  },
+  pwa: {
+    dest: 'public',
+  },
+});
